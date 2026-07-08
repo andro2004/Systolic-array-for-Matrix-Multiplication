449eb8c764999fd3e32c4634a7af140a116e18ff
andro2004 <138633335+andro2004@users.noreply.github.com>
2026-07-09 01:11:54 +0300

    Add files via upload

--- temp_registers_sequance.sv
+++ temp_registers_sequance.sv
@@ -1,23 +1,26 @@
-module temp_registers_sequance#(parameter DATAWIDTH=32,REG_COUNT=3) (data_in,data_out,clk,rst_n);
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 06/03/2026 03:55:51 PM
+// Design Name: 
+// Module Name: temp_registers_sequance
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
 
-input clk,rst_n;
-input [DATAWIDTH-1:0] data_in;
 
-output [DATAWIDTH-1:0] data_out;
+module temp_registers_sequance(
 
-wire [DATAWIDTH-1:0] regs_out[REG_COUNT+1];
-assign data_out = regs_out[REG_COUNT-1];
-
-generate 
-genvar i;
-	for(i=0;i<REG_COUNT;i=i+1)
-	begin:regs
-		if(i==0)
-			temp_register #(DATAWIDTH) temp_reg (data_in,regs_out[i],clk,rst_n);
-		else
-			temp_register #(DATAWIDTH) temp_reg (regs_out[i-1],regs_out[i],clk,rst_n);
-	end
-
-endgenerate
-
-endmodule 
+    );
+endmodule
